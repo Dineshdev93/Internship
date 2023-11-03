dcc2c3513e95529d100987d777dab010931916a1
Dinesh Sharma <dineshsharmadev93@gmail.com>
2023-11-03 20:46:24 +0530

    intial commit

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,9 +1,107 @@
-import React from 'react'
-import './home.css'
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Dropdown from "react-bootstrap/Dropdown";
+import "./home.css";
 export default function Home() {
+  //If you want to use icons then use font awesome icons and givd cdn link to index.html
   return (
-    <div>
-       Home Page
-    </div>
-  )
+    <section className="container ">
+      <div className="main mt-3 d-flex justify-content-between">
+        <Form inline>
+          <Row>
+            <Col xs="auto">
+              <Form.Control
+                type="text"
+                placeholder="Search"
+                className=" mr-sm-2 shadow"
+              />
+            </Col>
+            <Col xs="auto">
+              <Button variant="success">Search</Button>
+            </Col>
+          </Row>
+        </Form>
+        <div className="right">
+          <Button>
+            {" "}
+            <i class="fa-regular fa-plus"></i> Add User
+          </Button>
+        </div>
+      </div>
+      {/* export csv , gender,status */}
+      <div className="csv_gender_status d-flex justify-content-between mt-5">
+        <div className="export">
+          <Button variant="success">Export To Csv</Button>
+        </div>
+        <div className="gender">
+          <div>
+            <h3>Filter by gender</h3>
+          </div>
+          <div className="filter_gender d-flex justify-content-around">
+            <Form.Check
+              type={"radio"}
+              label={"All"}
+              name={"gender"}
+              value={"All"}
+              defaultChecked
+            />{" "}
+            <Form.Check
+              type={"radio"}
+              label={"Male"}
+              name={"gender"}
+              value={"Male"}
+            />{" "}
+            <Form.Check
+              type={"radio"}
+              label={"Female"}
+              name={"gender"}
+              value={"Female"}
+            />
+          </div>
+        </div>
+        {/* short by value */}
+        <div className="statu">
+          <h3>Short by Value</h3>
+          <Dropdown className="text-center">
+            <Dropdown.Toggle id="dropdown-basic">
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item >New data</Dropdown.Item>
+              <Dropdown.Item >Old data</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+        {/* filter by status */}
+        <div className="gender">
+          <div>
+            <h3>Filter by gender</h3>
+          </div>
+          <div className="filter_gender d-flex justify-content-around">
+            <Form.Check
+              type={"radio"}
+              label={"All"}
+              name={"status"}
+              value={"All"}
+              defaultChecked
+            />{" "}
+            <Form.Check
+              type={"radio"}
+              label={"Active"}
+              name={"status"}
+              value={"Active"}
+            />{" "}
+            <Form.Check
+              type={"radio"}
+              label={"Inactive"}
+              name={"status"}
+              value={"Inactive"}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,9 +1,51 @@
-import React from 'react'
-import './profile.css'
+import React from "react";
+import Card from "react-bootstrap/Card";
+import "./profile.css";
+import Row from "react-bootstrap/esm/Row";
 export default function Profile() {
   return (
-    <div>
-      Profile Page
+    <div className="container">
+      <Card className="shadow mt-5 col-lg-5 mx-auto">
+        <Card.Body>
+          <Row>
+            <div className="col">
+              <div className="card_profile d-flex justify-content-center">
+                <img src="/man.png" alt="" />
+              </div>
+            </div>
+          </Row>
+          <div className="text-center">
+            <h3>Dinesh Sharma</h3>
+            <h5>
+              <i class="fa-solid fa-envelope email "></i>&nbsp;:-{" "}
+              <span>mantry.dinesh@gmail.com</span>{" "}
+            </h5>
+            <h5>
+              <i class="fa-solid fa-phone mobile "></i>&nbsp;:- <span>8290868058</span>{" "}
+            </h5>
+            <h5>
+              <i class="fa-solid fa-person genders"></i>&nbsp;:- <span>Male</span>{" "}
+            </h5>
+            <h5>
+              <i class="fa-sharp fa-solid fa-location-dot location"></i>&nbsp;:-{" "}
+              <span>Location</span>{" "}
+            </h5>
+            <h5 className="status">
+              Status&nbsp;:- <span>Active</span>{" "}
+            </h5>
+            <h5>
+              {" "}
+              <i class="fa-solid fa-calendar-days date_created"></i>&nbsp;Date Created
+              &nbsp;:- <span>12-10-2023</span>{" "}
+            </h5>
+            <h5>
+              {" "}
+              <i class="fa-solid fa-calendar-days date_updated"></i>&nbsp;Date updated
+              &nbsp;:- <span>14-10-2023</span>{" "}
+            </h5>
+          </div>
+        </Card.Body>
+      </Card>
     </div>
-  )
+  );
 }
